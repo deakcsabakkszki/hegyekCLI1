644b9ac24350d802a9c139deef49c06000d8c509
qgedeak@gmail.com <qgedeak@gmail.com>
2026-03-04 07:42:58 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace hegyekCLI1
+{
+
+    public class Hegycsus
+    {
+       public string csucsNev {  get; private set; }
+       public string hegyseg { get; private set; }
+       public int magassag { get; private set; }
+
+        public Hegycsus(string csucsNev, string hegyseg, int magassag)
+        {
+            this.csucsNev = csucsNev;
+            this.hegyseg = hegyseg;
+            this.magassag = magassag;
+        }
+
+        public Hegycsus(string sor)
+        {
+            this.csucsNev = sor.Split(';')[0];
+            this.hegyseg = sor.Split(';')[1];
+            this.magassag = int.Parse(sor.Split(';')[2]);
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
